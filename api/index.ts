3cab77779c258c55fc80b0a52150963ec4b8f00e
000Ahsan <ahsankamran996@gmail.com>
2026-03-31 14:47:41 +0500

    feat: initialize backend core with Prisma schema, work log services, and Cloudinary integration

--- api/index.ts
+++
@@ -1,7 +0,0 @@
-import { createApp } from '../src/main';
-
-export default async (req: any, res: any) => {
-    const app = await createApp();
-    const instance = app.getHttpAdapter().getInstance();
-    return instance(req, res);
-};
--- src/common/common.module.ts
+++ src/common/common.module.ts
@@ -1,10 +1,12 @@
 import { Module, Global } from '@nestjs/common';
 import { PrismaService } from './prisma/prisma.service';
 import { FileService } from './file/file.service';
+import { CloudinaryModule } from './cloudinary/cloudinary.module';
 
 @Global()
 @Module({
+  imports: [CloudinaryModule],
   providers: [PrismaService, FileService],
-  exports: [PrismaService, FileService]
+  exports: [PrismaService, FileService, CloudinaryModule]
 })
 export class CommonModule { }
