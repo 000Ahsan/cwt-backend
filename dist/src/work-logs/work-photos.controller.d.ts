878356bfee110e8846be12454779c5ec32b8bbba
000Ahsan <ahsankamran996@gmail.com>
2026-03-31 10:39:16 +0500

    Remove dist folder from tracking

--- dist/src/work-logs/work-photos.controller.d.ts
+++
@@ -1,7 +0,0 @@
-import type { Response } from 'express';
-import { PrismaService } from '../common/prisma/prisma.service';
-export declare class WorkPhotosController {
-    private prisma;
-    constructor(prisma: PrismaService);
-    getPhoto(year: string, month: string, filename: string, req: any, res: Response): Promise<void>;
-}
